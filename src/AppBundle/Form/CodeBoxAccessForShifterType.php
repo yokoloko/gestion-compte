dbd3eaf75d3a676f55d00867eb77b15953dc1378
Yoann Laissus <yoann.laissus@gmail.com>
2019-02-16 12:21:24 +0100

    Minor design fixes

--- src/AppBundle/Form/CodeBoxAccessForShifterType.php
+++ src/AppBundle/Form/CodeBoxAccessForShifterType.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\CodeBoxAccessForShifter;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,12 +19,10 @@ class CodeBoxAccessForShifterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('afterDelay', NumberType::class, ['label' => 'Délai avant'])
-            ->add('beforeDelay', NumberType::class, ['label' => 'Délai après'])
+            ->add('afterDelay', IntegerType::class, ['label' => 'Délai avant'])
+            ->add('beforeDelay', IntegerType::class, ['label' => 'Délai après'])
             ->add('canGenerate', CheckboxType::class,  ['label' => 'Code générable', 'required' => false, 'attr' => ['class' => '']])
             ->add('slug', TextType::class, ['label' => 'Slug']);
-
-
     }
 
     /**
